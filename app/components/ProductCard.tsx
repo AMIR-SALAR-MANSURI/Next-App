950c00ea404a9f51662a7b4fd85085c9b39b8d32
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2023-10-21 17:29:25 +0330

    initail commit

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import AddTOCart from "./AddTOCart";
+
+function ProductCard() {
+  return (
+    <div>
+      <AddTOCart />
+    </div>
+  );
+}
+
+export default ProductCard;
---
+++ app/users/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+export default async function page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  const users: User[] = await res.json();
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
